d033b4ee789bf74aabdbb91513e9277efb2c5d4d
tedder <ted@adhoclabs.co>
2013-06-01 23:06:50 -0700

    superfund sites

---
+++ sourcer/superfund.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import requests
+import sys
+
+if not len(sys.argv) == 3:
+	print "pass json as arg plzkthzbai"
+
+req = requests.get("http://ofmpub.epa.gov/enviro/frs_rest_services.get_facilities", params={'program_output':'yes', 'output':'JSON', 'search_radius':'1', 'latitude83':'34.03', 'longitude83':'-118.2'})
+out = req.json()
+
+data = {}
+for site in out['Results']['FRSFacility']:
+	name = site['FacilityName']
+	ll = (site['Latitude83'], site['Longitude83'])
+	data[name] = {'coord':ll,'desc':''}
+
+with open(sys.argv[1], 'w') as f:
+	f.write(json.dumps(data, indent=2, ensure_ascii=False))
+	f.close()
