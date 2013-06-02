8cc6b93ab0fb4b713911544b8a807759bea59ed8
tedder <ted@adhoclabs.co>
2013-06-02 12:08:45 -0700

    all overlays loading, more superfund data.

--- sourcer/superfund.py
+++ sourcer/superfund.py
@@ -7,10 +7,12 @@ import json
 if not len(sys.argv) == 2:
 	print "pass json as arg plzkthzbai"
 
-req = requests.get("http://ofmpub.epa.gov/enviro/frs_rest_services.get_facilities", params={'program_output':'yes', 'output':'JSON', 'search_radius':'1', 'latitude83':'34.03', 'longitude83':'-118.2'})
+req = requests.get("http://ofmpub.epa.gov/enviro/frs_rest_services.get_facilities", params={'program_output':'yes', 'output':'JSON', 'search_radius':'2', 'latitude83':'34.03', 'longitude83':'-118.2'})
+print "url: %s" % req.url
 out = req.json()
 
 data = {}
+print "res: %s" % len(out['Results'])
 for site in out['Results']['FRSFacility']:
 	name = site['FacilityName']
 	ll = (site['Latitude83'], site['Longitude83'])
